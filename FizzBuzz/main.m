45fe8ceaf40dcaae478515719c4d1c30b2101e4b
Евгений Иванов <jovanny041@ya.ru>
2016-08-02 12:23:05 +0300

    Initial Commit

---
+++ FizzBuzz/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FizzBuzz
+//
+//  Created by Евгений Иванов on 02.08.16.
+//  Copyright © 2016 Евгений Иванов. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
